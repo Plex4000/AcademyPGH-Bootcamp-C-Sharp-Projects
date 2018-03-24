aa57bb9908e9920c406d4b35f6cc0db301f3c79a
Plex4000 <rajnarayanan2020@gmail.com>
2018-03-24 17:59:48 -0400

    Added 2 more exercises

---
+++ BattleShipExercise/BattleShipExercise/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var shipCount = 13;
+            string[,] shipGrid = new string[8, 8]
+                {
+                { "0", "0", "0", "0", "0", "0", "0", "0"},
+                { "0", "0", "0", "0", "*", "0", "0", "0"},
+                { "0", "0", "*", "*", "0", "0", "0", "0"},
+                { "0", "0", "0", "0", "0", "0", "0", "0"},
+                { "0", "0", "0", "*", "*", "0", "0", "0"},
+                { "*", "0", "*", "0", "0", "0", "0", "0"},
+                { "0", "0", "0", "0", "0", "0", "*", "*"},
+                { "0", "0", "*", "*", "*", "*", "0", "0"}
+                };
+
+            while (true)
+            {
+                if(shipCount == 0)
+                {
+                    Console.WriteLine("You won the game!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Which coordinate do you want to shoot?");
+
+                    string input = Console.ReadLine();
+                    string[] inputArray = input.Split();
+                    int[] intArray = Array.ConvertAll(inputArray, s => int.Parse(s));
+
+                    if (shipGrid[intArray[0], intArray[1]] == "*")
+                    {
+                        Console.WriteLine("Yay! you hit a ship!");
+                        shipGrid[intArray[0], intArray[1]] = "0";
+                        shipCount--;
+                    }
+                    else if (shipGrid[intArray[0], intArray[1]] != "*")
+                    {
+                        Console.WriteLine("You didn't hit a ship. Try again!");
+
+                        if (shipGrid[intArray[0] - 1, intArray[1]] != null && shipGrid[intArray[0]-1, intArray[1]] == "*")
+                        {
+                            Console.WriteLine("close!");
+                        }
+                        else if(shipGrid[intArray[0] + 1, intArray[1]] != null && shipGrid[intArray[0] + 1, intArray[1]] == "*")
+                        {
+                            Console.WriteLine("close!");
+                        }
+                        else if (shipGrid[intArray[0], intArray[1] + 1] != null && shipGrid[intArray[0], intArray[1] + 1] == "*")
+                        {
+                            Console.WriteLine("close!");
+                        }
+                        else if (shipGrid[intArray[0] + 1, intArray[1] - 1] != null && shipGrid[intArray[0] + 1, intArray[1] - 1] == "*")
+                        {
+                            Console.WriteLine("close!");
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MessageStorageRetrieval/MessageStorageRetrieval/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageStorageRetrieval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            string[] messages = new string[20];
+            string message;
+            int messageID;
+
+            while (true)
+            {
+                    Console.WriteLine("Would you like to [S]ave or [R]etrieve a message?");
+                    var input = Console.ReadLine().ToLower();
+
+                    while (input != "s" && input != "r")
+                    {
+                        Console.Write("please enter only s or r: ");
+                        input = Console.ReadLine();
+                    }
+                    if (input == "s")
+                    {
+                        
+                        if (count < 20)
+                        {
+                            
+                            
+                            Console.Write("What is your message? ");
+                            message = Console.ReadLine();
+                            messages[count] = message;
+                            count++;
+                        }
+                        else
+                        {
+                        Console.WriteLine("You can only add 20 messages!");
+                        } 
+                        
+                    }
+                    if (input == "r")
+                    {
+                        Console.Write("Which message would you like?");
+                        messageID = Convert.ToInt32(Console.ReadLine());
+                        if (messages[messageID] == null)
+                        {
+                        Console.WriteLine("There is no message to retrieve");
+                        }
+                    else
+                    {
+                        Console.WriteLine("Your message is: " + messages[messageID]);
+                    }
+                       
+                    }
+
+                }
+
+            }
+        }
+}
