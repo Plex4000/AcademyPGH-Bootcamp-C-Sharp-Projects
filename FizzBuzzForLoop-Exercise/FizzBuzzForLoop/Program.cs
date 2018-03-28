a9324367fee36e6e0a8effc785f4ecbd3f74d303
Plex4000 <rajnarayanan2020@gmail.com>
2018-03-28 18:57:29 -0400

    Modified to include a function

--- FizzBuzzForLoop-Exercise/FizzBuzzForLoop/Program.cs
+++ FizzBuzzForLoop-Exercise/FizzBuzzForLoop/Program.cs
@@ -12,23 +12,30 @@ namespace FizzBuzzForLoop
         {
             for (int i = 1; i <= 100; i++)
             {
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                    Console.WriteLine("fizzbuzz");
-                }
-                else if (i % 3 == 0)
-                {
-                    Console.WriteLine("fizz");
-                }
-                else if (i % 5 == 0)
-                {
-                    Console.WriteLine("buzz");
-                }
-                else
-                {
-                    Console.WriteLine(i);
-                }
+                PrintFizzBuzz(i);
             }
         }
+
+        public static void PrintFizzBuzz(int i)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("fizzbuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+            
     }
 }
