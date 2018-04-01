4015d36b9f9a851fa71de3cb825f35df965d02bf
Plex4000 <rajnarayanan2020@gmail.com>
2018-04-01 07:32:40 -0400

    Modified program.cs file

--- GameOfLife/GameOfLife/Program.cs
+++ GameOfLife/GameOfLife/Program.cs
@@ -13,19 +13,21 @@ namespace Conway
             //Create arrays for current state and next state arrays
             // "X" = dead cell
             // "O" = alive cell
-            int length = 10;
-            int height = 10;
-            string[,] carray = new string[height, length];
-            string[,] narray = new string[height, length];
+            Console.Write("How many rows do you want? ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.Write("How many rows do you want? ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            string[,] carray = new string[rows, length];
+            string[,] narray = new string[rows, length];
             Random roll = new Random();
-
+            int rand;
             //Fill up current array
-            for (int i = 0; i < height; i++)
+            for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < length; j++)
                 {
                     //Randomly pick alive or dead cell based on outcome of RNG of 0 or 1
-                    int rand = roll.Next(0, 2);
+                    rand = roll.Next(0, 2);
                     if (rand == 1)
                         carray[i, j] = "O";
                     else
@@ -37,7 +39,7 @@ namespace Conway
             while (true)
             {
                 //Print current array (pause after)
-                for (int i = 0; i < height; i++)
+                for (int i = 0; i < rows; i++)
                 {
                     for (int j = 0; j < length; j++)
                     {
@@ -45,108 +47,108 @@ namespace Conway
                     }
                     Console.WriteLine();
                 }
-                Console.ReadLine();
+
 
                 //Evaluate surrounding cells of current index
-                int count = 0;
-                for (int i = 0; i < height; i++)
+                int count;
+                for (int i = 0; i < rows; i++)
                 {
                     for (int j = 0; j < length; j++)
                     {
                         count = 0;
-
+                        
                         //left side
-                        if(j == 0 && i != 0 && i != 9)
+                        if (j == 0 && i != 0 && i != rows - 1)
                         {
-                            if (carray[i + 1, 0] == "0")
+                            if (carray[i + 1, 0] == "O")
                             {
                                 count++;
                             }
-                            if (carray[i, 1] == "0")
+                            if (carray[i, 1] == "O")
                             {
                                 count++;
                             }
-                            if (carray[i - 1, 0] == "0")
+                            if (carray[i - 1, 0] == "O")
                             {
                                 count++;
                             }
-                            if (carray[i + 1, 1] == "0")
+                            if (carray[i + 1, 1] == "O")
                             {
                                 count++;
                             }
-                            if (carray[i - 1, 1] == "0")
+                            if (carray[i - 1, 1] == "O")
                             {
                                 count++;
                             }
                         }
                         //right side
-                        else if (j == 9 && i != 0 && i != 9)
+                        else if (j == 4 && i != 0 && i != rows - 1)
                         {
-                            if (carray[i + 1, 9] == "0")
+                            if (carray[i + 1, length - 1] == "O")
                             {
                                 count++;
                             }
-                            if (carray[i, 8] == "0")
+                            if (carray[i, length - 2] == "O")
                             {
                                 count++;
                             }
-                            if (carray[i - 1, 9] == "0")
+                            if (carray[i - 1, length - 1] == "O")
                             {
                                 count++;
                             }
-                            if (carray[i + 1, 8] == "0")
+                            if (carray[i + 1, length - 2] == "O")
                             {
                                 count++;
                             }
-                            if (carray[i - 1, 8] == "0")
+                            if (carray[i - 1, length - 2] == "O")
                             {
                                 count++;
                             }
                         }
                         //top
-                        else if (i == 0 && j != 0 && j != 9)
+                        else if (i == 0 && j != 0 && j != length - 1)
                         {
-                            if (carray[0, j - 1] == "0")
+                            if (carray[0, j - 1] == "O")
                             {
                                 count++;
                             }
-                            if (carray[0, j + 1] == "0")
+                            if (carray[0, j + 1] == "O")
                             {
                                 count++;
                             }
-                            if (carray[1, j] == "0")
+                            if (carray[1, j] == "O")
                             {
                                 count++;
                             }
-                            if (carray[1, j - 1] == "0")
+                            if (carray[1, j - 1] == "O")
                             {
                                 count++;
                             }
-                            if (carray[1, j + 1] == "0")
+                            if (carray[1, j + 1] == "O")
                             {
                                 count++;
                             }
                         }
                         //bottom
-                        else if (i == 9 && j != 0 && j != 9)
+                        else if (i == rows - 1 && j != 0 && j != length - 1)
                         {
-                            if (carray[9, j - 1] == "0")
+                            if (carray[rows - 1, j - 1] == "O")
                             {
                                 count++;
                             }
-                            if (carray[9, j + 1] == "0")
+                            if (carray[rows, j + 1] == "O")
                             {
                                 count++;
                             }
-                            if (carray[8, j] == "0")
+                            if (carray[rows - 2, j] == "O")
                             {
                                 count++;
                             }
-                            if (carray[8, j - 1] == "0")
+                            if (carray[rows - 2, j - 1] == "O")
                             {
                                 count++;
                             }
-                            if (carray[8, j + 1] == "0")
+                            if (carray[rows - 2, j + 1] == "O")
                             {
                                 count++;
                             }
@@ -154,98 +156,99 @@ namespace Conway
                         //top left corner
                         else if (i == 0 && j == 0)
                         {
-                            if (carray[0, 1] == "0")
+                            if (carray[0, 1] == "O")
                             {
                                 count++;
                             }
-                            if (carray[1, 0] == "0")
+                            if (carray[1, 0] == "O")
                             {
                                 count++;
                             }
-                            if (carray[1, 1] == "0")
+                            if (carray[1, 1] == "O")
                             {
                                 count++;
                             }
                         }
                         //top right corner
-                        else if (i == 0 && j == 9)
+                        else if (i == 0 && j == length - 1)
                         {
-                            if (carray[0, 8] == "0")
+                            if (carray[0, length - 2] == "O")
                             {
                                 count++;
                             }
-                            if (carray[1, 9] == "0")
+                            if (carray[1, length - 1] == "O")
                             {
                                 count++;
                             }
-                            if (carray[1, 8] == "0")
+                            if (carray[1, length - 2] == "O")
                             {
                                 count++;
                             }
                         }
                         //bottom left corner
-                        else if (i == 9 && j == 0)
+                        else if (i == rows - 1 && j == 0)
                         {
-                            if (carray[9, 1] == "0")
+                            if (carray[rows - 1, 1] == "O")
                             {
                                 count++;
                             }
-                            if (carray[8, 0] == "0")
+                            if (carray[rows - 2, 0] == "O")
                             {
                                 count++;
                             }
-                            if (carray[8, 1] == "0")
+                            if (carray[rows - 2 , 1] == "O")
                             {
                                 count++;
                             }
                         }
                         //bottom right corner
-                        else if (i == 9 && j == 9)
+                        else if (i == rows - 1 && j == length - 1)
                         {
-                            if (carray[9, 8] == "0")
+                            if (carray[rows - 1, length - 2] == "O")
                             {
                                 count++;
                             }
-                            if (carray[8, 9] == "0")
+                            if (carray[rows - 2, length - 1] == "O")
                             {
                                 count++;
                             }
-                            if (carray[8, 8] == "0")
+                            if (carray[rows - 2, length - 2] == "O")
                             {
                                 count++;
                             }
                         }
+                        //all other cells
                         else
                         {
-                            if (carray[i, j - 1] == "0")
+                            if (carray[i, j - 1] == "O")
                             {
                                 count++;
                             }
-                            if (carray[i, j + 1] == "0")
+                            if (carray[i, j + 1] == "O")
                             {
                                 count++;
                             }
-                            if (carray[i + 1, j] == "0")
+                            if (carray[i + 1, j] == "O")
                             {
                                 count++;
                             }
-                            if (carray[i - 1, j] == "0")
+                            if (carray[i - 1, j] == "O")
                             {
                                 count++;
                             }
-                            if (carray[1 + 1, j - 1] == "0")
+                            if (carray[1 + 1, j - 1] == "O")
                             {
                                 count++;
                             }
-                            if (carray[1 + 1, j + 1] == "0")
+                            if (carray[1 + 1, j + 1] == "O")
                             {
                                 count++;
                             }
-                            if (carray[1 - 1, j - 1] == "0")
+                            if (carray[1 - 1, j - 1] == "O")
                             {
                                 count++;
                             }
-                            if (carray[1 - 1, j + 1] == "0")
+                            if (carray[1 - 1, j + 1] == "O")
                             {
                                 count++;
                             }
@@ -284,17 +287,50 @@ namespace Conway
          3.Any live cell with more than three live neighbors dies, as if by overpopulation
          4.Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction. */
 
+                        /* 
+                         1. live with < 2 live neighbors dies
+                         2. live with 2 or 3 live neighbors lives
+                         3. live with > 3 live neighbors dies
+                         4. dead with 3 live n becomes live cell
+                         
+                         */
+
                         //Determine next state for current index
-                        if (carray[i, j] == "O") //current index is alive
-                            if (count > 3)
-                                narray[j, i] = "X";//next state is dead
+                        if (carray[i, j] == "O")
+                        { //current index is alive
+                            if (count > 3 || count < 2)
+                            {
+                                narray[i, j] = "X";//next state is dead
+                            }
                             else
-                                narray[j, i] = "O";//next state is alive
-                        else if (carray[j, i] == "X")
+                            {
+                                narray[i, j] = "O";
+                            }
+
+                            if (count == 2 || count == 3)
+                            {
+                                narray[i, j] = "O";
+                            }
+                            else
+                            {
+                                narray[i, j] = "X";
+                            }
+                        }
+                   
+                        else if (carray[i, j] == "X")
+                        {
                             if (count == 3)
-                                narray[j, i] = "O";
+                            {
+                                narray[i, j] = "O";
+                            }
+
                             else
-                                narray[j, i] = "X";
+                            {
+                                narray[i, j] = "X";
+                            }
+                                
+                        }
+                           
 
 
 
@@ -302,13 +338,15 @@ namespace Conway
                 }//Evaluate current index loops
 
                 //Write the next state to the current state
-                for (int i = 0; i < height; i++)
+                for (int i = 0; i < rows; i++)
                 {
                     for (int j = 0; j < length; j++)
                     {
-                        carray[j, i] = narray[j, i];
+                        carray[i, j] = narray[i, j];
                     }
                 }
+
+                Console.ReadLine();
 
             }//Loop entire program
         } //Main
