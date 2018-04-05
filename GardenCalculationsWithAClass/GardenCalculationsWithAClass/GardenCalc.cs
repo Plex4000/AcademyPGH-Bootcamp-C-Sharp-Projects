b4c75414db5bc93a2a98f7664ff229a1566dbd41
Plex4000 <rajnarayanan2020@gmail.com>
2018-04-04 22:57:55 -0400

    Added the garden calculations with a class project

---
+++ GardenCalculationsWithAClass/GardenCalculationsWithAClass/GardenCalc.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenCalculationsWithAClass
+{
+   public class GardenCalc
+    {
+        public int Length;
+        public int Height;
+        public int Area;
+
+        public GardenCalc(int length, int height)
+        {
+            Length = length;
+            Height = height;
+        }
+
+        public int GetArea()
+        {
+            Area = Length * Height;
+            return Area;
+        }
+
+        public int GetPerimeter()
+        {
+            int perimeter = 2 * Length + 2 * Height;
+            return perimeter;
+        }
+
+        public double GetNumOfCarrots()
+        {
+            return Math.Ceiling((Area / 16.0) * 16);
+        }
+
+        public double GetNumOfCorn()
+        {
+            return Math.Ceiling((Area / 16.0) * 3);
+        }
+
+        public double GetNumOfBeets()
+        {
+            return Math.Ceiling((Area / 16.0) * 9);
+        }
+    }
+}
---
+++ GardenCalculationsWithAClass/GardenCalculationsWithAClass/Program.cs
@@ -0,0 +1,39 @@
+﻿using GardenCalculationsWithAClass;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenCalculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is the length and width of your garden in feet?: ");
+            string[] data = new string[2];
+            int[] units = new int[2];
+
+            data = Console.ReadLine().Split(' ');
+
+            units = Array.ConvertAll(data, int.Parse);
+
+            int length = units[0];
+            int height = units[1];
+
+            GardenCalc calc = new GardenCalc(length, height);
+            int Area = calc.GetArea();
+            int perimeter = calc.GetPerimeter();
+
+            Console.WriteLine($"The area of your garden is {Area}" );
+            Console.WriteLine($"The perimeter of your garden is {perimeter}.");
+            Console.WriteLine($"You can plant {calc.GetNumOfCarrots()} carrots.");
+            Console.WriteLine($"You can plant {calc.GetNumOfCorn()} ears of corn.");
+            Console.WriteLine($"You can plant {calc.GetNumOfBeets()} beets.");
+            Console.WriteLine("Congratulations! Now you can cook your vegetables! :)");
+
+        }
+    }
+}
