106ce4664e3b929892708ca144c97da1412844af
Plex4000 <rajnarayanan2020@gmail.com>
2018-04-08 12:35:29 -0400

    Modified program.cs file

--- FibonacciCalculation/FibonacciCalculation/Program.cs
+++ FibonacciCalculation/FibonacciCalculation/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FibonacciCalculation
 {
@@ -12,53 +10,33 @@ namespace FibonacciCalculation
         
         static void Main(string[] args)
         {
-            //Console.WriteLine(Fibonacci(10));
-            //Console.WriteLine(nums[9]);
-            //var result = FibonacciCalculations();
+            var result = FibonacciCalculations();
 
-            //for (int i = 0; i < result.Count(); i++)
-            //{
-            //    Console.Write(result[i] + " ");
-            //}
+            for (int i = 0; i < result.Count(); i++)
+            {
+                Console.Write(result[i] + " ");
+            }
         }
 
-        public static List<int> FibonacciCalculations()
+        public static List<long> FibonacciCalculations()
         {
-            int[] temp = new int[3];
-            List<int> fibonacci = new List<int>
-            {
-                0,
-                1
-            };
+            long[] temp = new long[2];
+            List<long> fibonacci = new List<long>();
+            fibonacci.Add(0);
+            fibonacci.Add(1);
             temp[0] = 0;
             temp[1] = 1;
-            int tempVar = 0;
-            int tempVar2 = 0;
-            for (int i = 0; i < 20; i++)
+            long tempVar = 0;
+            long tempVar2 = 0;
+
+            for (int i = 0; i < 48; i++)
             {
                 tempVar = temp[0] + temp[1];
                 fibonacci.Add(tempVar);
-                tempVar = tempVar + temp[1];
-                fibonacci.Add(tempVar);
                 temp[0] = temp[1];
-                temp[1] = tempVar;
+                temp[1] = tempVar;  
             }
             return fibonacci;
         }
-        //public static int Fibonacci(int number)
-        //{
-
-        //    if (number <= 1)
-        //    {
-        //        return 1;
-        //    }
-        //    else
-        //    {
-        //        nums.Add(Fibonacci(number - 2) + Fibonacci(number - 1));
-        //        return Fibonacci(number - 2) + Fibonacci(number - 1);
-
-        //    }
-        //}
-
     }
 }
