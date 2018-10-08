2fd750222296a0bc0e8ab51739ff6f0e32a19243
rajn2020 <rajnarayanan2020@gmail.com>
2018-10-08 18:11:41 -0400

    added palindrome project

---
+++ VerifyPalindrome/VerifyPalindrome/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VerifyPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a word to check if it is a palindrome");
+            var input = Console.ReadLine();
+
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            var revword = new string(charArray);
+
+            if (input == revword)
+            {
+                Console.WriteLine("The word is a palindrome!");
+            }
+            else
+            {
+                Console.WriteLine("The word is not a palindrome");
+            }
+
+        }
+    }
+}
